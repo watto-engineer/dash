1a2edecd2b76c275d8ca257f4e0b10f4bf4ed84a
FornaxA <fornaxa@servitising.org>
2020-02-03 12:51:41 +0100

    Tweak staking thread timing

--- src/init.cpp
+++ src/init.cpp
@@ -2379,7 +2379,7 @@ bool AppInitMain()
     }
 
     if (stakingManager->fEnableStaking) {
-        scheduler.scheduleEvery(boost::bind(&CStakingManager::DoMaintenance, boost::ref(stakingManager), boost::ref(*g_connman)), 1 * 1000);
+        scheduler.scheduleEvery(boost::bind(&CStakingManager::DoMaintenance, boost::ref(stakingManager), boost::ref(*g_connman)), 5 * 1000);
     }
 #endif // ENABLE_WALLET
 
