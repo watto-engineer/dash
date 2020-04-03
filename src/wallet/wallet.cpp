8f85c56971b2db8ef8b22a2fc6ba14dfacc0fe67
FornaxA <fornaxa@servitising.org>
2020-04-03 08:10:45 +0200

    Skip checking for duplicate inputs when adding zerocoin txs to the wallet
    As all zerocoin transactions have inputs with tx hash 0x0, all zerocoin transactions
    were marked as 'conflicted' due to having the same inputs as other transactions.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1257,7 +1257,7 @@ bool CWallet::AddToWalletIfInvolvingMe(const CTransactionRef& ptx, const CBlockI
             for (const CTxIn& txin : tx.vin) {
                 std::pair<TxSpends::const_iterator, TxSpends::const_iterator> range = mapTxSpends.equal_range(txin.prevout);
                 while (range.first != range.second) {
-                    if (range.first->second != tx.GetHash()) {
+                    if (range.first->second != tx.GetHash() && range.first->first.hash != uint256()) {
                         LogPrintf("Transaction %s (in block %s) conflicts with wallet transaction %s (both spend %s:%i)\n", tx.GetHash().ToString(), pIndex->GetBlockHash().ToString(), range.first->second.ToString(), range.first->first.hash.ToString(), range.first->first.n);
                         MarkConflicted(pIndex->GetBlockHash(), range.first->second);
                     }
