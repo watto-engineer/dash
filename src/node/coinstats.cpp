54a27d13dd0ffc766a6b202bff8e4168e71ea749
FornaxA <fornaxa@servitising.org>
2020-08-06 13:36:49 +0200

    Take zerocoin (deprecated) and unspendable coins into account in coin supply
    Modified by: wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>

--- src/node/coinstats.cpp
+++ src/node/coinstats.cpp
@@ -65,7 +65,10 @@ static void ApplyStats(CCoinsStats& stats, T& hash_obj, const uint256& hash, con
         ApplyHash(stats, hash_obj, hash, outputs, it);
 
         stats.nTransactionOutputs++;
-        stats.nTotalAmount += it->second.out.nValue;
+        const CScript& script = output.second.out.scriptPubKey;
+        if (script.size() > 0 && script.size() < MAX_SCRIPT_SIZE && *script.begin() != OP_RETURN && *script.begin() != OP_ZEROCOINMINT) {
+            stats.nTotalAmount += output.second.out.nValue;
+        }
         stats.nBogoSize += GetBogoSize(it->second.out.scriptPubKey);
     }
 }
