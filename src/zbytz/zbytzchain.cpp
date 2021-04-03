b59370e178e0a05dcc66327925f90c438631439c
celbalrai <80897309+celbalrai@users.noreply.github.com>
2021-04-03 12:20:36 +0200

    Update zerocoin - adapt to Bytz

--- src/zbytz/zbytzchain.cpp
+++ src/zbytz/zbytzchain.cpp
@@ -379,7 +379,7 @@ bool UpdateZBYTZSupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck
     std::list<libzerocoin::CoinDenomination> listSpends = ZerocoinSpendListFromBlock(block, fFilterInvalid);
 
     // Initialize zerocoin supply to the supply from previous block
-    if (pindex->pprev && pindex->pprev->GetBlockHeader().nVersion > 3) {
+    if (pindex->pprev && pindex->pprev->GetBlockHeader().nVersion == BLOCKHEADER_LEGACY_VERSION) {
         for (auto& denom : libzerocoin::zerocoinDenomList) {
             pindex->mapZerocoinSupply.at(denom) = pindex->pprev->GetZcMints(denom);
         }
