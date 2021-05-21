08484a114c9104ee301b2f7b7c475c723ac359dd
celbalrai <80897309+celbalrai@users.noreply.github.com>
2021-05-21 20:34:12 +0200

    Add include for boost::interruption_point

--- src/pos/staking-manager.cpp
+++ src/pos/staking-manager.cpp
@@ -18,6 +18,8 @@
 #include "validation.h"
 #include "wallet/wallet.h"
 
+#include <boost/thread.hpp>
+
 std::shared_ptr<CStakingManager> stakingManager;
 
 CStakingManager::CStakingManager(std::shared_ptr<CWallet> pwalletIn) :
