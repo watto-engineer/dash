ea8196ac9e8326fd0280b336eabc4698f9d0a904
BytzCK <bytzck@protonmail.com>
2021-05-29 14:31:32 +0100

    Update to build qt under gcc 10 and above, get cmake from github

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -1,6 +1,6 @@
 package=cmake
 $(package)_version=3.14.7
-$(package)_download_path=https://cmake.org/files/v3.14/
+$(package)_download_path=https://github.com/Kitware/CMake/releases/download/v3.14.7/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9221993e0af3e6d10124d840ff24f5b2f3b884416fca04d3312cb0388dec1385
 
