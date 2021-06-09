6fc3a3629216342d5daa23ef1417e9cce2f0831c
celbalrai <80897309+celbalrai@users.noreply.github.com>
2021-06-09 20:31:14 +0200

    Update params - set wait-for-islocked-timout to 4 minutes

--- src/llmq/quorums_chainlocks.h
+++ src/llmq/quorums_chainlocks.h
@@ -52,7 +52,7 @@ class CChainLocksHandler : public CRecoveredSigsListener
     static const int64_t CLEANUP_SEEN_TIMEOUT = 24 * 60 * 60 * 1000;
 
     // how long to wait for islocks until we consider a block with non-islocked TXs to be safe to sign
-    static const int64_t WAIT_FOR_ISLOCK_TIMEOUT = 10 * 60;
+    static const int64_t WAIT_FOR_ISLOCK_TIMEOUT = 4 * 60;
 
 private:
     CScheduler* scheduler;
