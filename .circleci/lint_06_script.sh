c4e1a51941c6c3a28399bb42401cc9161165c2f9
bytzck <82664078+bytzck@users.noreply.github.com>
2021-06-28 15:02:20 +0100

    Initial setup for CircleCI (#36)

---
+++ .circleci/lint_06_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C
+
+contrib/devtools/git-subtree-check.sh src/leveldb
+contrib/devtools/git-subtree-check.sh src/secp256k1
+contrib/devtools/git-subtree-check.sh src/crypto/ctaes
+contrib/devtools/git-subtree-check.sh src/univalue
+contrib/devtools/lint-whitespace.sh
