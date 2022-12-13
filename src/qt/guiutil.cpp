bytzck/BYTZ

Rebrand - additional renaming

src/qt/guiutil.cpp
@@ -869,8 +869,8 @@ fs::path static GetAutostartFilePath()
 {
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
-        return GetAutostartDir() / "bytzcore.desktop";
-    return GetAutostartDir() / strprintf("bytzcore-%s.lnk", chain);
+        return GetAutostartDir() / "bytz.desktop";
+    return GetAutostartDir() / strprintf("bytz-%s.lnk", chain);
 }
 
 bool GetStartOnSystemStartup()
@@ -910,7 +910,7 @@ bool SetStartOnSystemStartup(bool fAutoStart)
         if (!optionFile.good())
             return false;
         std::string chain = gArgs.GetChainName();
-        // Write a bytzcore.desktop file to the autostart directory:
+        // Write a bytz.desktop file to the autostart directory:
         optionFile << "[Desktop Entry]\n";
         optionFile << "Type=Application\n";
         if (chain == CBaseChainParams::MAIN)

watto-engineer/dash

src/qt/guiutil.cpp
@@ -883,8 +883,8 @@ fs::path static GetAutostartFilePath()
 {
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
-        return GetAutostartDir() / "wagerrcore.desktop";
-    return GetAutostartDir() / strprintf("wagerrcore-%s.desktop", chain);
+        return GetAutostartDir() / "wagerr.desktop";
+    return GetAutostartDir() / strprintf("wagerr-%s.desktop", chain);
 }
 
 bool GetStartOnSystemStartup()
@@ -924,7 +924,7 @@ bool SetStartOnSystemStartup(bool fAutoStart)
         if (!optionFile.good())
             return false;
         std::string chain = gArgs.GetChainName();
-        // Write a wagerrcore.desktop file to the autostart directory:
+        // Write a wagerr.desktop file to the autostart directory:
         optionFile << "[Desktop Entry]\n";
         optionFile << "Type=Application\n";
         if (chain == CBaseChainParams::MAIN)