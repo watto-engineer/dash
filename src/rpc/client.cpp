35344eb5f4521354a52d8b38136abf575de62ea7
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-12-20 13:52:20 +0000

    Remove coinjoin related RPC commands

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -168,8 +168,6 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setban", 2, "bantime" },
     { "setban", 3, "absolute" },
     { "setnetworkactive", 0, "state" },
-    { "setcoinjoinrounds", 0, "rounds" },
-    { "setcoinjoinamount", 0, "amount" },
     { "setwalletflag", 1, "value" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },
