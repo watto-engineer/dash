wagerr-from-builder/wagerr

Update make check tests
-Governance proposals json add correct mainnet address update key tests
-Update rpc_tests
-Update make check python tests
-Switch to quark for hashing
-Remove checkblock

src/bench/crypto_hash.cpp
@@ -154,60 +154,60 @@ static void HASH_DSHA256_2048b_single(benchmark::State& state)
         CHash256().Write(in.data(), in.size()).Finalize(in.data());
 }
 
-static void HASH_X11(benchmark::State& state)
+static void HASH_Quark(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(BUFFER_SIZE,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_0032b_single(benchmark::State& state)
+static void HASH_Quark_0032b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(32,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_0080b_single(benchmark::State& state)
+static void HASH_Quark_0080b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(80,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_0128b_single(benchmark::State& state)
+static void HASH_Quark_0128b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(128,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_0512b_single(benchmark::State& state)
+static void HASH_Quark_0512b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(512,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_1024b_single(benchmark::State& state)
+static void HASH_Quark_1024b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(1024,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
-static void HASH_X11_2048b_single(benchmark::State& state)
+static void HASH_Quark_2048b_single(benchmark::State& state)
 {
     uint256 hash;
     std::vector<uint8_t> in(2048,0);
     while (state.KeepRunning())
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQuark(in.begin(), in.end());
 }
 
 BENCHMARK(HASH_RIPEMD160, 440);
@@ -215,7 +215,7 @@ BENCHMARK(HASH_SHA1, 570);
 BENCHMARK(HASH_SHA256, 340);
 BENCHMARK(HASH_DSHA256, 340);
 BENCHMARK(HASH_SHA512, 330);
-BENCHMARK(HASH_X11, 500);
+BENCHMARK(HASH_Quark, 500);
 
 BENCHMARK(HASH_SHA256_0032b, 4 * 1000 * 1000);
 BENCHMARK(HASH_DSHA256_0032b, 2 * 1000 * 1000);
@@ -228,11 +228,11 @@ BENCHMARK(HASH_DSHA256_0128b_single, 1200 * 1000);
 BENCHMARK(HASH_DSHA256_0512b_single, 500 * 1000);
 BENCHMARK(HASH_DSHA256_1024b_single, 300 * 1000);
 BENCHMARK(HASH_DSHA256_2048b_single, 150 * 1000);
-BENCHMARK(HASH_X11_0032b_single, 70 * 1000);
-BENCHMARK(HASH_X11_0080b_single, 65 * 1000);
-BENCHMARK(HASH_X11_0128b_single, 60 * 1000);
-BENCHMARK(HASH_X11_0512b_single, 50 * 1000);
-BENCHMARK(HASH_X11_1024b_single, 50 * 1000);
-BENCHMARK(HASH_X11_2048b_single, 50 * 1000);
+BENCHMARK(HASH_Quark_0032b_single, 70 * 1000);
+BENCHMARK(HASH_Quark_0080b_single, 65 * 1000);
+BENCHMARK(HASH_Quark_0128b_single, 60 * 1000);
+BENCHMARK(HASH_Quark_0512b_single, 50 * 1000);
+BENCHMARK(HASH_Quark_1024b_single, 50 * 1000);
+BENCHMARK(HASH_Quark_2048b_single, 50 * 1000);
 BENCHMARK(FastRandom_32bit, 110 * 1000 * 1000);
 BENCHMARK(FastRandom_1bit, 440 * 1000 * 1000);

watto-engineer/dash

src/bench/crypto_hash.cpp
@@ -75,12 +75,12 @@ static void HASH_1MB_SHA3_256(benchmark::Bench& bench)
     });
 }
 
-static void HASH_1MB_X11(benchmark::Bench& bench)
+static void HASH_1MB_QUARK(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(BUFFER_SIZE,0);
     bench.batch(in.size()).unit("byte").minEpochIterations(10).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
@@ -140,59 +140,59 @@ static void HASH_DSHA256_2048b_single(benchmark::Bench& bench)
     });
 }
 
-/* Hash different number of bytes via X11 */
+/* Hash different number of bytes via QUARK */
 
-static void HASH_X11_0032b_single(benchmark::Bench& bench)
+static void HASH_QUARK_0032b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(32,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
-static void HASH_X11_0080b_single(benchmark::Bench& bench)
+static void HASH_QUARK_0080b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(80,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
-static void HASH_X11_0128b_single(benchmark::Bench& bench)
+static void HASH_QUARK_0128b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(128,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
-static void HASH_X11_0512b_single(benchmark::Bench& bench)
+static void HASH_QUARK_0512b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(512,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
-static void HASH_X11_1024b_single(benchmark::Bench& bench)
+static void HASH_QUARK_1024b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(1024,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
-static void HASH_X11_2048b_single(benchmark::Bench& bench)
+static void HASH_QUARK_2048b_single(benchmark::Bench& bench)
 {
     uint256 hash;
     std::vector<uint8_t> in(2048,0);
     bench.minEpochIterations(10000).run([&] {
-        hash = HashX11(in.begin(), in.end());
+        hash = HashQUARK(in.begin(), in.end());
     });
 }
 
@@ -299,7 +299,7 @@ BENCHMARK(HASH_1MB_SHA1);
 BENCHMARK(HASH_1MB_SHA256);
 BENCHMARK(HASH_1MB_SHA512);
 BENCHMARK(HASH_1MB_SHA3_256);
-BENCHMARK(HASH_1MB_X11);
+BENCHMARK(HASH_1MB_QUARK);
 
 BENCHMARK(HASH_DSHA256_0032b_single);
 BENCHMARK(HASH_DSHA256_0080b_single);
@@ -307,12 +307,12 @@ BENCHMARK(HASH_DSHA256_0128b_single);
 BENCHMARK(HASH_DSHA256_0512b_single);
 BENCHMARK(HASH_DSHA256_1024b_single);
 BENCHMARK(HASH_DSHA256_2048b_single);
-BENCHMARK(HASH_X11_0032b_single);
-BENCHMARK(HASH_X11_0080b_single);
-BENCHMARK(HASH_X11_0128b_single);
-BENCHMARK(HASH_X11_0512b_single);
-BENCHMARK(HASH_X11_1024b_single);
-BENCHMARK(HASH_X11_2048b_single);
+BENCHMARK(HASH_QUARK_0032b_single);
+BENCHMARK(HASH_QUARK_0080b_single);
+BENCHMARK(HASH_QUARK_0128b_single);
+BENCHMARK(HASH_QUARK_0512b_single);
+BENCHMARK(HASH_QUARK_1024b_single);
+BENCHMARK(HASH_QUARK_2048b_single);
 
 BENCHMARK(HASH_SHA256_32b);
 BENCHMARK(HASH_SipHash_32b);