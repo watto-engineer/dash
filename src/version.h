8d1658c7307d0f5daf5d0198bab2b536ac6d7678
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-12-21 11:29:01 +0000

    Add in wagerr specific values

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70930;
+static const int PROTOCOL_VERSION = 70929;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 214;
@@ -20,25 +20,25 @@ static const int INIT_PROTO_VERSION = 214;
 static const int GETHEADERS_VERSION = 70238;
 
 //! New spork protocol
-static const int MIN_SPORK_VERSION = 70930;
+static const int MIN_SPORK_VERSION = 70926;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 70923;
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70928;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70929;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70926;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70928;
 
 //! minimum proto version of masternode to accept in DKGs
-static const int MIN_MASTERNODE_PROTO_VERSION = 70928;
+static const int MIN_MASTERNODE_PROTO_VERSION = 70926;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 70928;
+static const int CADDR_TIME_VERSION = 66666;
 
 //! protocol version is included in MNAUTH starting with this version
-static const int MNAUTH_NODE_VER_VERSION = 70928;
+static const int MNAUTH_NODE_VER_VERSION = 70926;
 
 //! introduction of QGETDATA/QDATA messages
-static const int LLMQ_DATA_MESSAGES_VERSION = 70928;
+static const int LLMQ_DATA_MESSAGES_VERSION = 70926;
 
 //! introduction of instant send deterministic lock (ISDLOCK)
 static const int ISDLOCK_PROTO_VERSION = 70920;
