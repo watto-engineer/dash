81ab5a61b5f2d35918286b487358fa439e26e11b
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2022-12-30 15:28:01 +0000

    Change copyrights

--- src/pos/rewards.h
+++ src/pos/rewards.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2020 The ION Core developers
-// Copyright (c) 2021 The Wagerr Core developers
+// Copyright (c) 2018-2022 The Dash Core developers
+// Copyright (c) 2014-2022 The Wagerr developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
--- src/pos/staking-manager.h
+++ src/pos/staking-manager.h
@@ -1,3 +1,4 @@
+// Copyright (c) 2018-2022 The Dash Core developers
 // Copyright (c) 2019-2020 The ION Core developers
 // Copyright (c) 2022 The Wagerr developers
 // Distributed under the MIT software license, see the accompanying
