3def2291d0408783f2212dfa43710e211626d9ea
Watto Engineer <109853210+watto-engineer@users.noreply.github.com>
2022-12-30 10:23:47 +0100

    Remove permissioned masternode model

--- src/evo/providertx.h
+++ src/evo/providertx.h
@@ -202,8 +202,7 @@ public:
         REASON_TERMINATION_OF_SERVICE = 1,
         REASON_COMPROMISED_KEYS = 2,
         REASON_CHANGE_OF_KEYS = 3,
-        REASON_EXPIRED = 4,
-        REASON_LAST = REASON_EXPIRED
+        REASON_LAST = REASON_CHANGE_OF_KEYS
     };
 
     uint16_t nVersion{CURRENT_VERSION}; // message version
