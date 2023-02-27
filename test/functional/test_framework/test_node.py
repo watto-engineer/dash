dc2cdde65642a886da7ffbf453d682900fc3a543
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-27 15:06:37 +0000

    Switch to generate from generatetoaddress in test node 2

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -276,9 +276,9 @@ class TestNode():
             time.sleep(1.0 / poll_per_s)
         self._raise_assertion_error("Unable to connect to wagerrd")
 
-    def generate(self, nblocks):
-        self.log.debug("TestNode.generate() dispatches `generate` call to `generatetoaddress`")
-        return self.generate(nblocks=nblocks)
+    #def generate(self, nblocks):
+    #    self.log.debug("TestNode.generate() dispatches `generate` call to `generatetoaddress`")
+    #    return self.generate(nblocks=nblocks)
 
     def get_wallet_rpc(self, wallet_name):
         if self.use_cli:
