a11f874389a9106648e7f90f4ac1a624ed2ccbe5
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-28 12:05:00 +0000

    Add breakpoint to test_node

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -159,6 +159,7 @@ class TestNode():
     ]
 
     def get_deterministic_priv_key(self):
+        breakpoint()
         """Return a deterministic priv key in base58, that only depends on the node's index"""
         assert len(self.PRIV_KEYS) == MAX_NODES
         return self.PRIV_KEYS[self.index]
