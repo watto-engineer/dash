a04f00572a50c6b18cf67169c6761a13097f16c8
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-02 11:29:51 +0000

    Add rawtransaction fee in betting_opcode 4

--- test/functional/test_framework/betting_opcode.py
+++ test/functional/test_framework/betting_opcode.py
@@ -248,7 +248,7 @@ def post_opcode(node, opcode, address):
     # Create the raw transaction.
     trx = node.createrawtransaction(inputs, outputs)
     # Add a fee rate
-    node.fundrawtransaction(trx['hex'], '{"feeRate":"0.003"')
+    node.fundrawtransaction(trx, {'feeRate':'0.03'})
     # Sign the raw transaction.
     trx = node.signrawtransactionwithwallet(trx)
     return node.sendrawtransaction(trx['hex'])
