38e6b1f646473bf4c2df82db386c7bd565ef804f
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-07 15:31:22 +0000

    Add generation for 5 blocks to mature the bets

--- test/functional/feature_betting.py
+++ test/functional/feature_betting.py
@@ -943,6 +943,7 @@ class BettingTest(WagerrTestFramework):
         listbets = self.nodes[0].listbetsdb(False)
 
         # generate block with payouts
+        blockhash = self.nodes[0].generate(5)
         blockhash = self.nodes[0].generate(1)[0]
         block = self.nodes[0].getblock(blockhash)
         height = block['height']
@@ -957,8 +958,8 @@ class BettingTest(WagerrTestFramework):
         player1_balance_after = Decimal(self.nodes[2].getbalance())
         player2_balance_after = Decimal(self.nodes[3].getbalance())
         #breakpoint()
-        #assert_equal((player1_balance_before + player1_expected_win - Decimal('406.8')), (player1_balance_after))
-        #assert_equal((player2_balance_before + player2_expected_win - Decimal('406.8')), (player2_balance_after))
+        assert_equal((player1_balance_before + player1_expected_win - Decimal('406.8')), (player1_balance_after))
+        assert_equal((player2_balance_before + player2_expected_win - Decimal('406.8')), (player2_balance_after))
 
         self.log.info("Total Bets Success")
 
